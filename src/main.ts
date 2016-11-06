fb16a2174d1cf8c76a901c4434789dd4822788be
Adrian Punga <adrian.punga@gmail.com>
2016-11-06 09:44:45 +0200

    Optimized method calls with ACK & Added connection state listener

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ export type CodeFunction = (returnMethod: ReturnFunction, args: any[]) => void;
 export type EventFunction = (...args: any[]) => void;
 
 class RemoteError extends Error {
+  stack: any;
   constructor(thrownException: any) {
     super(thrownException.message);
     this.name = thrownException.remote_name;
@@ -12,49 +13,47 @@ class RemoteError extends Error {
     this.stack = thrownException.remote_stack;
   }
 }
-interface CookieAndArgs {
-  cookie: string;
-  args: any[];
-}
 interface ReturnedValueAndThrownException {
   returnedValue: any;
   thrownException: any;
 }
 
-function S4(): string {
-  // tslint:disable-next-line no-bitwise
-  return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-}
-function GUID(): string {
-  return `${S4()}${S4()}-${S4()}-4${S4().substr(0,3)}-${S4()}-${S4()}${S4()}${S4()}`;
+export interface SioRpcClientConnectionStatusListener {
+  onConnectionStatusChange(status: boolean): void;
 }
 
 export class SioRpcClient {
 
   socket: SocketIOClient.Socket;
 
-  constructor(url: string) {
+  constructor(url: string, connectionStatusListener?: SioRpcClientConnectionStatusListener) {
     this.socket = ioclient.connect(url);
+    if (typeof connectionStatusListener !== 'undefined') {
+      this.socket.on('disconnect', () => {
+        connectionStatusListener.onConnectionStatusChange(false);
+      });
+      this.socket.on('connect', () => {
+        connectionStatusListener.onConnectionStatusChange(true);
+      });
+    }
   }
   call(methodName: string, ...args: any[]): Promise<any> {
     return new Promise((resolve, reject) => {
-      const cookie = GUID();
-      this.socket.once(`${methodName}..return..${cookie}`, (returnedValueAndThrownException: ReturnedValueAndThrownException) => {
+      this.socket.emit(methodName, args, (returnedValueAndThrownException: ReturnedValueAndThrownException) => {
         if (typeof returnedValueAndThrownException.thrownException !== 'undefined') {
           return reject(new RemoteError(returnedValueAndThrownException.thrownException));
         }
         resolve(returnedValueAndThrownException.returnedValue);
       });
-      this.socket.emit(`${methodName}..call`, <CookieAndArgs>{ cookie, args });
     });
   }
   subscribe(eventName: string, eventCode: EventFunction) {
-    this.socket.on(`${eventName}..event`, (args: any[]) => {
+    this.socket.on(eventName, (args: any[]) => {
       eventCode.apply(null, args);
     });
   }
   subscribeOnce(eventName: string, eventCode: EventFunction) {
-    this.socket.once(`${eventName}..event`, (args: any[]) => {
+    this.socket.once(eventName, (args: any[]) => {
       eventCode.apply(null, args);
     });
   }
